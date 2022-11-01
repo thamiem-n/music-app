cb606c5962c04e04c6c8c8590cb5464a51c1efae
ahmed <ahmedmuhammed78613@gmail.com>
2022-11-01 17:16:51 +0000

    added img

--- src/Data.js
+++ src/Data.js
@@ -1,10 +1,25 @@
-
-const artistData = ([
-    { name: "Eminem", age: 50, genre: "rap", album: ["Kamikaze", "Relapse", "The Eminem Show"]},
-    { name: "Central Cee", age: 24, genre: "rap", album: ["WildWest", "23"]},
-    { name: "Bob Marley", age: 36, genre: "reggae", album: ["Trench Town Rock", "Legends"]}
-]);
-
-
+const artistData = [
+	{
+		name: "Eminem",
+		age: 50,
+		genre: "Rap",
+		album: ["Kamikaze", "Relapse", "The Eminem Show"],
+		img: "https://media.npr.org/assets/music/news/2010/06/eminem-52893634f7c035450a7b5ff2e040cacb4012395e-s1100-c50.jpg",
+	},
+	{
+		name: "Central Cee",
+		age: 24,
+		genre: "Rap",
+		album: ["WildWest", "23"],
+		img: "https://akamai.sscdn.co/uploadfile/letras/fotos/6/0/c/0/60c0b4b2168bf8f932b7775ec388431b.jpg",
+	},
+	{
+		name: "Bob Marley",
+		age: 36,
+		genre: "Reggae",
+		album: ["Trench Town Rock", "Legends"],
+		img: "https://media.gq.com/photos/57b347617c4c5fcf03df62df/4:3/w_1500,h_1125,c_limit/bob-marley-gq-style-0616-lede.jpg",
+	},
+];
 
 export default artistData;
